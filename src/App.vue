5cc88463dcfda09b7f8228a34e02c360132433dc
叶子 <xiaoyexiang@126.com>
2019-01-10 09:33:10 +0800

    第一阶段完成

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+  html, body {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+  #app {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 10;
+    width: 100%;
+    height: 100%;
+    background: #f4f6f8;
+  }
+</style>
